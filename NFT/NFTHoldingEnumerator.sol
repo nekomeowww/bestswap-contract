24ec728149ca5ec7c96019819fffaea939cad56b
神樹桜乃 <kodama@sakuno.moe>
2020-10-05 14:36:03 +0800

    Add approved property to enumerator result

--- NFT/NFTHoldingEnumerator.sol
+++ NFT/NFTHoldingEnumerator.sol
@@ -9,6 +9,7 @@ contract NFTHoldingEnumerator {
     struct HoldingInfo {
         uint256 tokenId;
         uint256 quality;
+        address approved;
     }
 
     function getHoldingsOf(NFT nft, address account) external view returns (HoldingInfo[] memory result) {
@@ -18,6 +19,7 @@ contract NFTHoldingEnumerator {
         for (uint256 i = 0; i < count; i++) {
             result[i].tokenId = nft.tokenOfOwnerByIndex(account, i);
             result[i].quality = nft.qualityOf(result[i].tokenId);
+            result[i].approved = nft.getApproved(result[i].tokenId);
         }
     }
 }
