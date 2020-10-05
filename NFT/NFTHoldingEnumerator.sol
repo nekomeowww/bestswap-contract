cc419e7e72bf0931d4a5a51108f1d6625d58c82d
神樹桜乃 <kodama@sakuno.moe>
2020-10-05 11:03:21 +0800

    Add NFTHoldingEnumerator

---
+++ NFT/NFTHoldingEnumerator.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: None
+
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "./NFT.sol";
+
+contract NFTHoldingEnumerator {
+    struct HoldingInfo {
+        uint256 tokenId;
+        uint256 quality;
+    }
+
+    function getHoldingsOf(NFT nft, address account) external view returns (HoldingInfo[] memory result) {
+        uint256 count = nft.balanceOf(account);
+        result = new HoldingInfo[](count);
+
+        for (uint256 i = 0; i < count; i++) {
+            result[i].tokenId = nft.tokenOfOwnerByIndex(account, i);
+            result[i].quality = nft.qualityOf(result[i].tokenId);
+        }
+    }
+}
