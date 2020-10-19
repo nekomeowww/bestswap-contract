30893813a17a4af851c093c5c6a7ec12af39d69a
lychees <lychees67@gmail.com>
2020-10-19 02:13:04 -0700

    🔨 Only owner()

--- contracts/Mint/StakingRewardsFactory.sol
+++ contracts/Mint/StakingRewardsFactory.sol
@@ -10,14 +10,14 @@ interface IStakingRewards {
     function notifyRewardAmount(uint256 reward) external;
 }
 
-contract StakingRewardsFactory {
+contract StakingRewardsFactory is Ownable {
 
     address constant ref = address(0x04A90Bc01B23fe1656abE6c3CC1aBC92d4A27324);
     address constant rewardToken = address(0x36eb1b02cB7Be3ffA1eE7Bd2A3c7D036002730F7);
 
     // deploy a staking reward contract for the staking token, and store the reward amount
     // the reward will be distributed to the staking reward contract no sooner than the genesis
-    function new_pool(address stakingToken, uint amount) public {
+    function new_pool(address stakingToken, uint amount) onlyOwner() public {
         address pool = address(new StakingRewards(rewardToken, stakingToken, ref));
         IRef(ref).set_admin(pool);
         IERC20 token = IERC20(rewardToken);
