2b26010347612c3cdf0decec6a65c87d791fbf7d
Ayaka Neko <neko@ayaka.moe>
2020-10-19 16:28:14 +0800

    :sparkles: Resolve issue with ref interface and added constructor

--- contracts/Mint/StakingRewardsFactory.sol
+++ contracts/Mint/StakingRewardsFactory.sol
@@ -6,22 +6,22 @@
 pragma solidity ^0.6;
 import "./StakingRewards.sol";
 
-interface IRef {
-    function set_referrer(address r) external;
-    function add_score(uint d) external;
-    function set_admin(address a) external;
-}
-
 contract StakingRewardsFactory {
 
     address constant ref = address(0x04A90Bc01B23fe1656abE6c3CC1aBC92d4A27324);
-    address constant rewardToken = address(0x36eb1b02cb7be3ffa1ee7bd2a3c7d036002730f7);
+    address constant rewardToken = address(0x36eb1b02cB7Be3ffA1eE7Bd2A3c7D036002730F7);
+
+    IRef public refContract;
+
+    constructor () public {
+        refContract = IRef(ref);
+    }
 
     // deploy a staking reward contract for the staking token, and store the reward amount
     // the reward will be distributed to the staking reward contract no sooner than the genesis
-    function new_pool(address stakingToken, uint amount) {
+    function new_pool(address stakingToken, uint amount) public {
         address pool = address(new StakingRewards(rewardToken, stakingToken, ref));
-        ref.set_admin(pool);
+        refContract.set_admin(pool);
         IERC20 token = IERC20(rewardToken);
         token.transfer(pool, amount);
         pool.notifyRewardAmount(amount);
