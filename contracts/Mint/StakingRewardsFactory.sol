219d1546b734003a05b887609e63ea989bd2f3d7
Ayaka Neko <neko@ayaka.moe>
2020-10-22 18:17:25 +0800

    :sparkles: Added active_pool function to active pool

--- contracts/Mint/StakingRewardsFactory.sol
+++ contracts/Mint/StakingRewardsFactory.sol
@@ -18,6 +18,10 @@ contract StakingRewardsFactory is Ownable {
     address constant ref = address(0x75b82728C5a4f1CbfdC8b27C7DCFfea9bBa3F613);
     address constant rewardToken = address(0x36eb1b02cB7Be3ffA1eE7Bd2A3c7D036002730F7);
 
+    function active_pool(address poolAddress, uint amount) onlyOwner() public {
+        IStakingRewards(poolAddress).notifyRewardAmount(amount);
+    }
+
     // deploy a staking reward contract for the staking token, and store the reward amount
     // the reward will be distributed to the staking reward contract no sooner than the genesis
     function new_pool(address stakingToken, address yToken, uint amount) onlyOwner() public {
@@ -25,7 +29,6 @@ contract StakingRewardsFactory is Ownable {
         IRef(ref).set_admin(pool);
         IERC20 token = IERC20(rewardToken);
         token.transfer(pool, amount);
-        IStakingRewards(pool).notifyRewardAmount(amount);
     }
 
     function new_accelerator(address VESTtoken, address pool) onlyOwner() public {
